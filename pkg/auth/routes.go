1f3c9778bc70585917301453a792f15cc6d224f0
tranquangvan-tohmatsu <tran.quangvan@tohmatsu.co.jp>
2023-05-17 16:53:54 +0700

    Start grpc

---
+++ pkg/auth/routes.go
@@ -0,0 +1,26 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tranquangvan/go-grpc-api-gateway/pkg/auth/routes"
+	"github.com/tranquangvan/go-grpc-api-gateway/pkg/config"
+)
+
+func RegisterRoutes(r *gin.Engine, c *config.Config) *ServiceClient {
+	svc := &ServiceClient{
+		Client: InitServiceClient(c),
+	}
+
+	routes := r.Group("/auth")
+	routes.POST("/register", svc.Register)
+	routes.POST("/login", svc.Login)
+	return svc
+}
+
+func (svc *ServiceClient) Register(ctx *gin.Context) {
+	routes.Register(ctx, svc.Client)
+}
+
+func (svc *ServiceClient) Login(ctx *gin.Context) {
+	routes.Login(ctx, svc.Client)
+}
---
+++ pkg/auth/routes/login.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tranquangvan/go-grpc-api-gateway/pkg/auth/pb"
+)
+
+type LoginRequestBody struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func Login(ctx *gin.Context, c pb.AuthServiceClient) {
+	b := LoginRequestBody{}
+	if err := ctx.BindJSON(&b); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	res, err := c.Login(ctx, &pb.LoginRequest{
+		Email:    b.Email,
+		Password: b.Password,
+	})
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+	ctx.JSON(http.StatusCreated, &res)
+}
